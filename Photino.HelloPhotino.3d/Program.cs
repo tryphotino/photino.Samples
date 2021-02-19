d24fbcb8db4cd6d21b01e9bf0788bc51f656227e
ottodobretsberger <39414503+ottodobretsberger@users.noreply.github.com>
2021-02-19 15:34:15 +0100

    added sample project Photino3D

---
+++ Photino.HelloPhotino.3d/Program.cs
@@ -0,0 +1,20 @@
+﻿using PhotinoNET;
+using System;
+
+namespace HelloPhotino.ThreeD
+{
+    class Program
+    {
+        [STAThread]
+        static void Main(string[] args)
+        {
+            var window = new PhotinoWindow("Hello Photino 3D!", options => { });
+            window.NavigateToLocalFile("wwwroot/index.html");
+
+            window.Width = 2048;
+            window.Height = 1080;
+
+            window.WaitForExit();
+        }
+    }
+}

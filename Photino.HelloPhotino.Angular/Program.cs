07940e7b012e5ed3bf9cb3c262e12b9806142033
Philipp Bauer <hello@philippbauer.org>
2022-09-20 11:13:53 -0500

    Update Angular Sample to use Photino StaticFileServer

--- Photino.HelloPhotino.Angular/Program.cs
+++ Photino.HelloPhotino.Angular/Program.cs
@@ -1,57 +1,59 @@
-﻿using PhotinoNET;
-using System;
-using System.IO;
-using System.Text;
+﻿using System.Text;
+using PhotinoNET;
 
-namespace HelloPhotinoAngular
+namespace Photino.HelloPhotino.Angular;
+
+class Program
 {
-    class Program
+    [STAThread]
+    static void Main(string[] args)
     {
-        [STAThread]
-        static void Main(string[] args)
-        {
-            // Window title declared here for visibility
-            string windowTitle = "Photino.Angular Demo App";
+        PhotinoServer
+            .CreateStaticFileServer(args, out string baseUrl)
+            .RunAsync();
 
-            // Creating a new PhotinoWindow instance with the fluent API
-            var window = new PhotinoWindow()
-                .SetTitle(windowTitle)
-                // Resize to a percentage of the main monitor work area
-                //.Resize(50, 50, "%")
-                // Center window in the middle of the screen
-                .Center()
-                // Users can resize windows by default.
-                // Let's make this one fixed instead.
-                .SetResizable(false)
-                .RegisterCustomSchemeHandler("app", (object sender, string scheme, string url, out string contentType) =>
-                {
-                    contentType = "text/javascript";
-                    return new MemoryStream(Encoding.UTF8.GetBytes(@"
+        // Window title declared here for visibility
+        string windowTitle = "Photino.Angular Demo App";
+
+        // Creating a new PhotinoWindow instance with the fluent API
+        var window = new PhotinoWindow()
+            .SetTitle(windowTitle)
+            // Resize to a percentage of the main monitor work area
+            //.Resize(50, 50, "%")
+            // Center window in the middle of the screen
+            .Center()
+            // Users can resize windows by default.
+            // Let's make this one fixed instead.
+            .SetResizable(true)
+            .RegisterCustomSchemeHandler("app", (object sender, string scheme, string url, out string contentType) =>
+            {
+                contentType = "text/javascript";
+                return new MemoryStream(Encoding.UTF8.GetBytes(@"
                         (() =>{
                             window.setTimeout(() => {
                                 alert(`🎉 Dynamically inserted JavaScript.`);
                             }, 1000);
                         })();
                     "));
-                })
-                // Most event handlers can be registered after the
-                // PhotinoWindow was instantiated by calling a registration 
-                // method like the following RegisterWebMessageReceivedHandler.
-                // This could be added in the PhotinoWindowOptions if preferred.
-                .RegisterWebMessageReceivedHandler((object sender, string message) => {
-                    var window = (PhotinoWindow)sender;
+            })
+            // Most event handlers can be registered after the
+            // PhotinoWindow was instantiated by calling a registration 
+            // method like the following RegisterWebMessageReceivedHandler.
+            // This could be added in the PhotinoWindowOptions if preferred.
+            .RegisterWebMessageReceivedHandler((object sender, string message) =>
+            {
+                var window = (PhotinoWindow)sender;
 
-                    // The message argument is coming in from sendMessage.
-                    // "window.external.sendMessage(message: string)"
-                    string response = $"Received message: \"{message}\"";
+                // The message argument is coming in from sendMessage.
+                // "window.external.sendMessage(message: string)"
+                string response = $"Received message: \"{message}\"";
 
-                    // Send a message back the to JavaScript event handler.
-                    // "window.external.receiveMessage(callback: Function)"
-                    window.SendWebMessage(response);
-                })
-                .Load("wwwroot/index.html"); // Can be used with relative path strings or "new URI()" instance to load a website.
+                // Send a message back the to JavaScript event handler.
+                // "window.external.receiveMessage(callback: Function)"
+                window.SendWebMessage(response);
+            })
+            .Load($"{baseUrl}/index.html"); // Can be used with relative path strings or "new URI()" instance to load a website.
 
-            window.WaitForClose(); // Starts the application event loop
-        }
+        window.WaitForClose(); // Starts the application event loop
     }
 }

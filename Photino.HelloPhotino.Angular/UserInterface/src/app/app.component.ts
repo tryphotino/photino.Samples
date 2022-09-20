2123f7992301009d8724e22df2c50dfaf8a46da2
Philipp Bauer <hello@philippbauer.org>
2022-09-20 11:10:21 -0500

    Add Angular Sample

---
+++ Photino.HelloPhotino.Angular/UserInterface/src/app/app.component.ts
@@ -0,0 +1,37 @@
+import { Component } from '@angular/core';
+
+const API_URL = `https://api.github.com/repos/tryphotino/photino.NET/commits?per_page=3&sha=`;
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  branches = ['master', 'debug'];
+  currentBranch: string | undefined;
+  commits: any[] = [];
+
+  ngOnInit() {
+    this.selectBranch(this.branches[0]);
+  }
+
+  async selectBranch(branch: string) {
+    this.currentBranch = branch;
+    this.commits = await this.fetchCommits(branch);
+  }
+
+  async fetchCommits(branch: string) {
+    const url = `${API_URL}${this.currentBranch}`;
+    return await (await fetch(url)).json();
+  }
+
+  truncate(v: string) {
+    const newline = v.indexOf("\n");
+    return newline > 0 ? v.slice(0, newline) : v;
+  }
+
+  formatDate(v: string) {
+    return v.replace(/T|Z/g, " ");
+  }
+}

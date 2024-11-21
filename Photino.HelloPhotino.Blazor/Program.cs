6bf6b88977fcd7b2817158e934f7a280958c8a81
josago97 <josago2009@hotmail.es>
2024-11-21 13:05:45 +0100

    Add Blazor sample

---
+++ Photino.HelloPhotino.Blazor/Program.cs
@@ -0,0 +1,35 @@
+using Microsoft.Extensions.DependencyInjection;
+using Photino.Blazor;
+
+namespace HelloPhotino.Blazor
+{
+    public class Program
+    {
+        [STAThread]
+        static void Main(string[] args)
+        {
+            var appBuilder = PhotinoBlazorAppBuilder.CreateDefault(args);
+
+            appBuilder.Services
+                .AddLogging();
+
+            // register root component and selector
+            appBuilder.RootComponents.Add<App>("app");
+
+            var app = appBuilder.Build();
+
+            // customize window
+            app.MainWindow
+                .SetIconFile("favicon.ico")
+                .SetTitle("Photino Blazor Sample");
+
+            AppDomain.CurrentDomain.UnhandledException += (sender, error) =>
+            {
+                app.MainWindow.ShowMessage("Fatal exception", error.ExceptionObject.ToString());
+            };
+
+            app.Run();
+
+        }
+    }
+}
